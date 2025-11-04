281ab03298e0325704e7f772434fe81c08645b22
Sasta-Sharabi <153608735+Sasta-Sharabi@users.noreply.github.com>
2025-11-04 10:31:57 +0530

    set up server actions and event details page

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,27 @@
 import EventCard from '@/components/EventCard'
 import ExploreBtn from '@/components/ExploreBtn'
-import { events } from '@/lib/constants'
+import { IEvent } from '@/database/event.model';
+// import { events } from '@/lib/constants'
 
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
-const Home = () => {
+const Page = async () => {
+  const response = await fetch(`${BASE_URL}/api/events`);
+  const {events} = await response.json();
+  
   return (
     <section>
       <h1 className='text-center'>The hub for Every dev<br/>Event You Can't Miss</h1>
       <p className='text-center mt-5'>Hackathons, Meetups, and Conferences, All in One Place</p>
 
       <ExploreBtn />
-
+      
       <div className='mt-20 space-y-7'>
         <h3> 
           Featured Events
         </h3>
-        <ul className='events'>
-          {events.map((event)=>(
+        <ul className='events list-none'>
+          {events && events.length > 0 && events.map((event: IEvent)=>(
             <li key={event.title}>
               <EventCard {...event} />
             </li>
@@ -28,4 +33,4 @@ const Home = () => {
   )
 }
 
-export default Home
+export default Page
